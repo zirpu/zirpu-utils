31a1530d2014916dd3fe0b037d3aa1d2791c9d7d
allan bailey <zirpubolci@gmail.com>
2016-09-30 06:34:33 -0700

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     description="Zirpu's Misc. Utilities.",
     long_description=open(HERE + '/README.rst').read(),
 
-    version='0.2.0',
+    version='0.3.0',
 
     author='Allan Bailey',
     author_email='zirpubolci@gmail.com',
